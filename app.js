8d75384947b9ceccc5bde70254bcc932cd425ee5
vicky1337 <vikrantrana1993@hotmail.com>
2018-12-30 23:10:48 +0530

    routes update

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 
-
 const app = express();
+
+// Routes
+
+app.use('/' , require('./routes/index'));
+app.use('/users' , require('./routes/users'));
+
 
 const PORT = process.env.PORT || 5000;
 
--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,13 @@
-    
+const express = require('express');
+const router =  express.Router();
+
+//Login Page  
+router.get('/login' , (req, res) => res.send('Welcome Vicky1337!'));
+
+
+
+//Register Page  
+router.get('/register' , (req, res) => res.send('Register'));
+
+
+module.exports = router; 
