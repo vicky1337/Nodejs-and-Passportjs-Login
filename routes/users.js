a9718a2f87fb16cee61c717585460f8a8c0a1141
vicky1337 <vikrantrana1993@hotmail.com>
2019-01-01 15:28:42 +0530

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router =  express.Router();
+const bcrypt = require('bcryptjs');
+
+// User model
+const User = require('../models/User');
+
+
 
 //Login Page  
 router.get('/login' , (req, res) => res.render('login'));
-
 
 
 //Register Page  
@@ -43,7 +48,50 @@ router.post('/register' , (req, res) => {
             password2   
         });
     }else{
-        res.send('pass');
+        
+        //validation Passed
+        User.findOne({ email: email})
+        .then(user => {
+            if(user){
+                //User Exists
+                errors.push({ msg: 'Email is already Registered'});
+                res.render('register', {
+                    errors, 
+                    name,
+                    email,
+                    password,
+                    password2   
+                });
+
+            } else {
+               
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                
+                }); 
+               // Hash Password
+
+               bcrypt.genSalt(10 , (err , salt) => 
+               bcrypt.hash(newUser.password, salt, (err,hash) => {
+
+                if(err) throw err;
+                // Set Password to hashed
+                newUser.password = hash;
+
+                //Save the User
+
+                newUser.save()
+                .then(user => {
+                    res.redirect('users/login');
+                })
+                .catch(err => console.log(err));
+
+               
+               }))
+            }
+        });
     }
 });
 
